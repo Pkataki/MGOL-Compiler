14e1d4c5b635fe2bc6fdb94b3c309f423ff69677
Pkataki <paulogkataki@hotmail.com>
2018-04-26 17:13:38 -0300

    final version

--- lexic_analyzer2.rb
+++ lexic_analyzer2.rb
@@ -5,101 +5,127 @@ require 'constants_lexic'
 
 class Lexic_Analyzer
 
-	# attr_accessor :states, :final_states
 	SIZEBYTES = 1
 
 	def get_next_token
 
-		name_lexeme = ""
-		actual_state = 0 
+		name_lexeme = "" 
+		current_state = 0 
 		last_state = 0
 		has_error = 0
 		
-		
 		while @found_eof == false
 			
-			char = IO.read(@file,SIZEBYTES,@offset)
-			#print char + "\n"
-			if TYPE::is_eof(char) and name_lexeme.size == 0
+			#reading one byte at a time
+			char = IO.read(@file,SIZEBYTES,@offset) 
+	
+			if TYPE::is_eof(char) 
+
+				#checking literal and comentary error	
+				if @states[current_state]["final"].nil? 
+					
+					error_state = @states[current_state]["transitions"].values.first
+				
+					type_error = @states[error_state]["token"] == TOKENS::LITERAL ? "literal": "comentary"
+
+					#raising exception  
+					@error_handling.lexic_error(char,"Invalid " + type_error, @first_apperance_line - @repeated_breakline,@first_apperance_column)
+			
+				end
 
 				char = "EOF"
 				
 				@found_eof = true
 				
-				last_state = @states[actual_state]["transitions"][char]
+				last_state = @states[0]["transitions"][char]
 				
-				actual_state = @states[last_state]["transitions"] # always retur nil
+				#always return nil
+				current_state = @states[last_state]["transitions"] 
 
 				name_lexeme = char
 			
 			else
-				
+
+				#updating lines and columns if occur some error
 				if char == "\n"
-					if @has_endline == false
-						@has_endline = true
-						@line_error += 1
-						@columns_error = 0
-					end
-				else
-					@has_endline = false
+					@line_error += 1
+					@columns_error = 0
 				end
-				
-				type_char = TYPE::get_type(char)
 
-				last_state = actual_state
+				# getting type, can be "EOF", "LETTER", "DIGIT"
+				type_char = TYPE::get_type(char) 
+
+				last_state = current_state
+
+				# checking transitions of the actual state
+				if not @states[current_state]["transitions"].nil?
 	
-				if @states[actual_state]["transitions"].nil? == false
-					
-					if @states[actual_state]["transitions"][type_char].nil?
-					#	print " ** " + name_lexeme + "\n"
-						if @states[actual_state]["self_loop"].nil?
-							actual_state = nil
-						end 
-					
+					if @states[current_state]["transitions"][type_char].nil?
+						
+						# checking if a state is a self loop
+						if @states[current_state]["self_loop"].nil? 
+							current_state = nil
+						end
+
+					# going to next state in the DFA
 					else
-						actual_state = @states[actual_state]["transitions"][type_char]
+						@first_apperance_line = @line_error
+						@first_apperance_column = @columns_error
+						current_state = @states[current_state]["transitions"][type_char] 
 					end
 				
 				else
-					actual_state = nil
+					current_state = nil
 				end
 
-				@offset += 1
+				# offset on file
+				@offset += 1 
 			end
-	
-			if actual_state.nil?
+
+			#don't have transition
+			if current_state.nil?
 
 				name_lexeme = name_lexeme.strip
 				
-				if @states[last_state]["final"] == true
+				#last_state is a final state
+				if @states[last_state]["final"] == true 
 					
 					@offset -= 1
+
+
+					if char == "\n"
+						@repeated_breakline += 1
+					end
 					
-					if name_lexeme.size > 0 and $symbol_table.has_key?(name_lexeme) == false and @states[last_state]["token"] == TOKENS::ID 
+					#checking if the lexeme read has the token "ID"
+					if not TYPE::is_eof(name_lexeme)  and not $symbol_table.has_key?(name_lexeme)  and @states[last_state]["token"] == TOKENS::ID 
 						
 						$symbol_table[name_lexeme] = Element.new :token => @states[last_state]["token"], :lexeme => name_lexeme	
 					
 					end
 
+					# returning the element if it's in the symbol table
 					if $symbol_table.has_key?(name_lexeme) == true
 						return  $symbol_table[name_lexeme]
+					
+					#return the element that is not in the symbol table
 					else
 						return Element.new :token => @states[last_state]["token"], :lexeme => name_lexeme
 					end
 
+				# an invalid lexeme appeared
 				else
-					@error_handling.lexic_error(char,"Invalid lexeme",@line_error,@columns_error)
+					@error_handling.lexic_error(char,"Invalid lexeme",@line_error - @repeated_breakline,@columns_error)
 				end	
 				
 				break
 			end
 
-			if char.nil? == false
-				name_lexeme += char
-			end
+			# appending the new character read in the lexeme name
+			name_lexeme += char
 		
+			# updating columns
 			@columns_error += 1
-			
 			
 		end
 
@@ -107,11 +133,15 @@ class Lexic_Analyzer
 
 	def initialize(file,transitions_file)
 
+		@repeated_breakline = 0
+
 		@offset = 0
 
 		@found_eof = false
 
-		@has_endline = false
+		@first_apperance_line = 0
+
+		@first_apperance_column = 0
 
 		@file = file
 		
@@ -121,7 +151,7 @@ class Lexic_Analyzer
 
 		@error_handling = Error_handle.new
 
-		@states = JSON.parse(File.read(transitions_file), object_class: OpenStruct)
+		@states = JSON.parse(File.read(transitions_file))
 		
 	end
 
@@ -131,6 +161,7 @@ end
 
 def create_symbol_table(reserved_words_file)
 
+	#creating symbol table
 	reserved_words = JSON.parse(File.read(reserved_words_file))
 		
 	symbol_table = Hash.new 
@@ -164,7 +195,7 @@ if __FILE__ == $0
 
 	while a = LA.get_next_token
 		
-		print "#{a.lexeme}  #{a.token}\n"
+		print "#{a.lexeme}  #{a.token}\n\n"
 		if TYPE::is_eof(a.lexeme)
 			break
 		end
